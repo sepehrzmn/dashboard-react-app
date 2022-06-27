5d7b2e2d4810d5f1e56e7a49372539048e882086
sepehrzmn <95043491+sepehrzmn@users.noreply.github.com>
2022-06-27 11:41:21 +0430

    added layout main and routes required

--- src/App.js
+++ src/App.js
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import "./App.css";
 
 const App = () => {
-	return <h1 className="underline text-3xl">App</h1>;
+	const activeMenu = true;
+	return (
+		<div>
+			<BrowserRouter>
+				<div className="flex relative dark:bg-main-dark-bg">
+					<div className="fixed right-4 bottom-4" style={{ zIndex: "10000" }}>
+						<TooltipComponent content="Setting" position="top">
+							<button
+								type="button"
+								className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white bg-[blue] rounded-full"
+							>
+								<FiSettings />
+							</button>
+						</TooltipComponent>
+					</div>
+					{activeMenu ? (
+						<div className="w-72 fixed sidebar bg-white dark:bg-secondary-dark-bg">
+							sidebar
+						</div>
+					) : (
+						<div className="w-0 dark:bg-secondary-dark-bg">sidebar w-0</div>
+					)}
+					<div
+						className={`bg-main-bg min-h-screen ${
+							activeMenu ? "md:ml-72" : "basis-2"
+						}`}
+					>
+						<div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+							Navbar
+						</div>
+					</div>
+					<div>
+						<Routes>
+							{/*  Dashboard */}
+							<Route path="/" element="ECommerce" />
+							<Route path="/ecommerce" element="ECommerce" />
+
+							{/* Pages */}
+							<Route path="/orders" element="Orders" />
+							<Route path="/employees" element="Employees" />
+							<Route path="/customers" element="Customers" />
+
+							{/* Apps */}
+							<Route path="/kanban" element="Kanban" />
+							<Route path="/editor" element="Editor" />
+							<Route path="/calendar" element="Kanban" />
+							<Route path="/color-picker" element="colorPicker" />
+
+							{/* charts */}
+							<Route path="/line" element="Line" />
+							<Route path="/area" element="Area" />
+							<Route path="/bar" element="Bar" />
+							<Route path="/pie" element="Pie" />
+							<Route path="/financial" element="Financial" />
+							<Route path="/color-mapping" element="colorMapping" />
+							<Route path="/pyramid" element="Pyramid" />
+							<Route path="/stacked" element="Stacked" />
+						</Routes>
+					</div>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
