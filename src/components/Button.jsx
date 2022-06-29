f9d863c50c0a1eaf149220ae73e00d32028f9665
sepehrzmn <95043491+sepehrzmn@users.noreply.github.com>
2022-06-29 12:34:01 +0430

    update button componet

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Button = () => {
-	return <div>Button</div>;
+const Button = ({ color, bgColor, text, borderRadius, size }) => {
+  return (
+    <button
+      className={`bg-[${bgColor}] text-${color} rounded-[${borderRadius}] text-${size} p-3 hover:drop-shadow-xl`}
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.propsTypes = {
+  color: PropTypes.string,
+  bgColor: PropTypes.string,
+  text: PropTypes.string,
+  borderRadius: PropTypes.string,
+  size: PropTypes.string,
 };
 
 export default Button;
