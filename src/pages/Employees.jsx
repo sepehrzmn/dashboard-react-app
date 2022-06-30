64bcca1f3970cb354bdffa9fc15ab20234a72303
sepehrzmn <95043491+sepehrzmn@users.noreply.github.com>
2022-06-30 10:32:21 +0430

    updated Employees page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Inject,
+  Search,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
+
+import { employeesData, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Employees = () => {
-	return <div>Employees</div>;
+  return (
+    <div className="m-3 md:m-10 p-4 rounded-3xl bg-white">
+      <Header category="Pages" title="Employees" />
+      <GridComponent id="gridComp" dataSource={employeesData} allowPaging toolbar={["Search"]}>
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Employees;
