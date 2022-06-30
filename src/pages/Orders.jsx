34471bf4a7f2ec089e7939c7d9e2c73648f96366
sepehrzmn <95043491+sepehrzmn@users.noreply.github.com>
2022-06-30 10:31:54 +0430

    updated Order page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Orders = () => {
-	return <div>Orders</div>;
+  return (
+    <div className="m-3 md:m-10 p-4 rounded-3xl bg-white">
+      <Header category="Pages" title="Orders" />
+      <GridComponent
+        id="gridComp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+        allowExcelExport
+        allowPdfExport
+        editSettings={{ allowEditing: true, allowDeleting: true }}
+        contextMenuItems={contextMenuItems}
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Resize, Sort, Filter, Edit, ExcelExport, PdfExport, ContextMenu]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Orders;
