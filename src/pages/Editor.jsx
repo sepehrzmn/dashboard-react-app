7a9ca0db61ca3f563cadadb84b47c2cbbf3f07a0
sepehrzmn <95043491+sepehrzmn@users.noreply.github.com>
2022-06-30 16:21:31 +0430

    updated Editor page

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+
+import { EditorData } from "../data/dummy";
+import { Header } from "../components";
 
 const Editor = () => {
-	return <div>Editor</div>;
+  const services = [HtmlEditor, Image, Link, QuickToolbar, Toolbar];
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-xl">
+      <Header category="App" title="Editor" />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={services} />
+      </RichTextEditorComponent>
+    </div>
+  );
 };
 
 export default Editor;
